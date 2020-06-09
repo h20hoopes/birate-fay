c3d6ee922685026fa374f551df48c24b44f95830
h20hoopes <64933169+h20hoopes@users.noreply.github.com>
2020-06-09 11:55:02 -0600

    placed large nav in one file

---
+++ js/full-nav.js
@@ -0,0 +1,31 @@
+$(function() {
+    $("#navigation").append("" +
+        "<div class=\"w3-bar w3-dark-grey w3-centered w3-hide-small w3-small\">\n" +
+        "    <a href=\"../index.html\" class=\"w3-bar-item w3-button\">Home</a>\n" +
+        "    <div class=\"w3-dropdown-hover\">\n" +
+        "        <button class=\"w3-button\">Personal</button>\n" +
+        "        <div class=\"w3-dropdown-content w3-bar-block w3-border\">\n" +
+        "            <a href=\"Warp96.html\" class=\"w3-bar-item w3-button\">Warp 96</a>\n" +
+        "            <a href=\"http://www.manabeadwork.com\" class=\"w3-bar-item w3-button\">Mana Beadwork</a>\n" +
+        "            <a href=\"MusicVideos.html\" class=\"w3-bar-item w3-button\">Music Videos</a>\n" +
+        "            <a href=\"FutureProjects.html\" class=\"w3-bar-item w3-button\">Future Projects</a>\n" +
+        "        </div>\n" +
+        "    </div>\n" +
+        "    <div class=\"w3-dropdown-hover\">\n" +
+        "        <button class=\"w3-button\">Professional</button>\n" +
+        "        <div class=\"w3-dropdown-content w3-bar-block w3-border\">\n" +
+        "            <a href=\"Research.html\" class=\"w3-bar-item w3-button\">Research</a>\n" +
+        "            <a href=\"DataAnalytics.html\" class=\"w3-bar-item w3-button\">Data Analytics</a>\n" +
+        "            <a href=\"WebDesign.html\" class=\"w3-bar-item w3-button\">Web Design</a>\n" +
+        "            <a href=\"QAEngineer.html\" class=\"w3-bar-item w3-button\">QA Engineer</a>\n" +
+        "            <a href=\"Service.html\" class=\"w3-bar-item w3-button\">Service</a>\n" +
+        "            <a href=\"RelevantClasses.html\" class=\"w3-bar-item w3-button\">Relevant Classes</a>\n" +
+        "            <a href=\"ITSecurity.html\" class=\"w3-bar-item w3-button\">IT Security</a>\n" +
+        "            <a href=\"Database.html\" class=\"w3-bar-item w3-button\">Database</a>\n" +
+        "        </div>\n" +
+        "    </div>\n" +
+        "    <a href=\"Blog.html\" class=\"w3-bar-item w3-button\">Blog</a>\n" +
+        "    <a href=\"Contact.html\" class=\"w3-bar-item w3-button\">Contact</a>\n" +
+        "    <a href=\"Experimental.html\" class=\"experimental w3-bar-item w3-button w3-hover-black\">Experimental</a>\n" +
+        "</div>");
+});
---
+++ js/index-full-nav.js
@@ -0,0 +1,31 @@
+$(function() {
+    $("#index-navigation").append("" +
+        "    <div class=\"w3-bar w3-dark-grey w3-centered w3-hide-small w3-small\">\n" +
+        "    <a href=\"index.html\" class=\"w3-bar-item w3-button\">Home</a>\n" +
+        "    <div class=\"w3-dropdown-hover\">\n" +
+        "        <button class=\"w3-button\">Personal</button>\n" +
+        "        <div class=\"w3-dropdown-content w3-bar-block w3-border\">\n" +
+        "            <a href=\"html/Warp96.html\" class=\"w3-bar-item w3-button\">Warp 96</a>\n" +
+        "            <a href=\"http://www.manabeadwork.com\" class=\"w3-bar-item w3-button\">Mana Beadwork</a>\n" +
+        "            <a href=\"html/MusicVideos.html\" class=\"w3-bar-item w3-button\">Music Videos</a>\n" +
+        "            <a href=\"html/FutureProjects.html\" class=\"w3-bar-item w3-button\">Future Projects</a>\n" +
+        "        </div>\n" +
+        "    </div>\n" +
+        "    <div class=\"w3-dropdown-hover\">\n" +
+        "        <button class=\"w3-button\">Professional</button>\n" +
+        "        <div class=\"w3-dropdown-content w3-bar-block w3-border\">\n" +
+        "            <a href=\"html/Research.html\" class=\"w3-bar-item w3-button\">Research</a>\n" +
+        "            <a href=\"html/DataAnalytics.html\" class=\"w3-bar-item w3-button\">Data Analytics</a>\n" +
+        "            <a href=\"html/WebDesign.html\" class=\"w3-bar-item w3-button\">Web Design</a>\n" +
+        "            <a href=\"html/QAEngineer.html\" class=\"w3-bar-item w3-button\">QA Engineer</a>\n" +
+        "            <a href=\"html/Service.html\" class=\"w3-bar-item w3-button\">Service</a>\n" +
+        "            <a href=\"html/RelevantClasses.html\" class=\"w3-bar-item w3-button\">Relevant Classes</a>\n" +
+        "            <a href=\"html/ITSecurity.html\" class=\"w3-bar-item w3-button\">IT Security</a>\n" +
+        "            <a href=\"html/Database.html\" class=\"w3-bar-item w3-button\">Database</a>\n" +
+        "        </div>\n" +
+        "    </div>\n" +
+        "    <a href=\"html/Blog.html\" class=\"w3-bar-item w3-button\">Blog</a>\n" +
+        "    <a href=\"html/Contact.html\" class=\"w3-bar-item w3-button\">Contact</a>\n" +
+        "    <a href=\"html/Experimental.html\" class=\"experimental w3-bar-item w3-button w3-hover-black\">Experimental</a>\n" +
+        "</div>");
+});
