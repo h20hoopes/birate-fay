095b81f5f43b4ff517f293c5dfb563ab226bc301
h20hoopes <64933169+h20hoopes@users.noreply.github.com>
2020-06-11 16:00:15 -0600

    universal background

---
+++ js/background.js
@@ -0,0 +1,9 @@
+$(function() {
+    $("#index-background").append("" +
+        "<img src=\"content/images/cool-background.png\" style=\"position: fixed; z-index: -1; min-width: 100%; min-height: 100%\">");
+});
+
+$(function() {
+    $("#background").append("" +
+        "<img src=\"../content/images/cool-background.png\" style=\"position: fixed; z-index: -1; min-width: 100%; min-height: 100%\">");
+});
